7e441e9b44a9b881a447dff07aa1a7415b9282f8
t4n9332 <t4n9332@gmail.com>
2026-08-20 08:40:06 +0900

    Update layout.tsx

--- ipsomun/app/layout.tsx
+++ ipsomun/app/layout.tsx
@@ -20,6 +20,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"
         />
+        {/* Google Analytics (GA4) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-21K65KFJ2J"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-21K65KFJ2J');`,
+          }}
+        />
       </head>
       <body>
         <header className="site-header">
